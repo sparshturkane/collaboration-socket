7272260ed6b9140a816df76950366a552fd91186
sparsh <rajaturkane99@gmail.com>
2017-12-09 17:11:25 +0530

    added new server listern codes

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var path = require('path');
+var io = require('socket.io')(server);
 var app = express();
 var server = require('http').createServer(app);
 var port = process.env.PORT || 4000;
@@ -7,5 +9,5 @@ server.listen(port, function () {
   console.log('Server listening at port %d', port);
 });
 
-// Static files
-app.use(express.static('public'));
+// Routing
+app.use(express.static(path.join(__dirname, 'public')));
